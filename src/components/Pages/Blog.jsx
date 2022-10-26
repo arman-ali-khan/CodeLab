6ade8b8552227665dbd6c60c49b73825464d1a51
Arman Khan <sparmankhan@gmail.com>
2022-10-26 16:34:39 +0600

    what is node js answer added

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -38,7 +38,14 @@ It supports authentication using passwords, phone numbers, popular federated ide
 The current logged in user (authUser) is retrieved from Redux state with a call to the useSelector() hook. Redux is used in this example however it is not required to implement a Private Route component in React Router 6. You could use a different state management library or any approach you prefer to get the logged in status of the user.</p>
 				</div>
 			</details>
-			
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">What is Node? How does Node work?</summary>
+				<div className="px-4 pb-4 space-y-2">
+					<p>Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive. <br /> <br />
+
+Working of Node.js: Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request.</p>
+				</div>
+			</details>
 		</div>
 	</div>
 </section>
