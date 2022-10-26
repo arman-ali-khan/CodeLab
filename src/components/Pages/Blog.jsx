a62486d50ed42276879a15e0e60de838cd394203
Arman Khan <sparmankhan@gmail.com>
2022-10-26 16:29:14 +0600

    what is cors queations ans added

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            Blog
+           <section className="bg-gray-100 text-gray-800">
+	<div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+		<h2 className="mb-12 text-3xl font-bold leading-none text-center sm:text-5xl">Questions and Answer</h2>
+		<div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-300">
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">What is cors?</summary>
+				<div className="px-4 pb-4">
+					<p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request. <br/><br/>
+
+An example of a cross-origin request: the front-end JavaScript code served from https://domain-a.com uses XMLHttpRequest to make a request for https://domain-b.com/data.json. <br/><br/>
+
+For security reasons, browsers restrict cross-origin HTTP requests initiated from scripts. For example, XMLHttpRequest and the Fetch API follow the same-origin policy. This means that a web application using those APIs can only request resources from the same origin the application was loaded from unless the response from other origins includes the right CORS headers.</p>
+				</div>
+			</details>
+			
+		</div>
+	</div>
+</section>
         </div>
     );
 };
